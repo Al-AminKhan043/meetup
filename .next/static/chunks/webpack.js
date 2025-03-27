a38e0e07e23e80569b198d1c6fa8b8186a06c321
Al Amin <alaminkhan10009@gmail.com>
2025-03-28 02:06:50 +0600

    fix gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce686a9464dc1a73"; }
+/******/ 		__webpack_require__.h = function() { return "c154a9125968bc79"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
